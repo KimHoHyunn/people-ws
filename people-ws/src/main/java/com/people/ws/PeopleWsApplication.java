afb81a649c519484f3712d61d6926505c64a9ef6
mh042 <mh042@192.168.249.40>
2022-12-14 13:52:37 +0900

    Web Socket

---
+++ people-ws/src/main/java/com/people/ws/PeopleWsApplication.java
@@ -0,0 +1,13 @@
+package com.people.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleWsApplication.class, args);
+	}
+
+}
